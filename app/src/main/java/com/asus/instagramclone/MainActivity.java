015313fe33482bc089b323f69e7682893ed87c5c
Omer Eyibardakci <omereyibardakci@gmail.com>
2023-07-26 13:59:57 +0300

    Initial commit

---
+++ app/src/main/java/com/asus/instagramclone/MainActivity.java
@@ -0,0 +1,26 @@
+package com.asus.instagramclone;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.os.Bundle;
+import android.view.View;
+
+import com.asus.instagramclone.databinding.ActivityMainBinding;
+
+public class MainActivity extends AppCompatActivity {
+
+    private ActivityMainBinding binding;
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        binding = ActivityMainBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+
+    }
+
+
+}
