17b5bf9df5252049b03f3d818400a227913546e4
Omer Eyibardakci <omereyibardakci@gmail.com>
2023-07-27 12:39:16 +0300

    Sign up and Sign in completed

--- app/src/main/java/com/asus/instagramclone/FeedActivity.java
+++ app/src/main/java/com/asus/instagramclone/FeedActivity.java
@@ -1,16 +1,50 @@
 package com.asus.instagramclone;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.Menu;
+import android.view.MenuInflater;
+import android.view.MenuItem;
+
+import com.google.firebase.auth.FirebaseAuth;
 
 public class FeedActivity extends AppCompatActivity {
 
+    FirebaseAuth mAuth;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_feed);
+
+        mAuth=FirebaseAuth.getInstance();
     }
 
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        MenuInflater menuInflater = getMenuInflater();
+        menuInflater.inflate(R.menu.option_menu,menu);
+        return super.onCreateOptionsMenu(menu);
+    }
 
+    @Override
+    public boolean onOptionsItemSelected(@NonNull MenuItem item) {
+        if(item.getItemId()==R.id.add_post){
+            Intent intentToShare =  new Intent(FeedActivity.this,ShareActivity.class);
+            startActivity(intentToShare);
+
+        } else if (item.getItemId()==R.id.log_out) {
+
+            mAuth.signOut();
+
+            Intent intentToMain = new Intent(FeedActivity.this,MainActivity.class);
+            intentToMain.addFlags(Intent.FLAG_ACTIVITY_CLEAR_TOP); // closes all previous activities
+            startActivity(intentToMain);
+
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
 }
--- app/src/main/java/com/asus/instagramclone/ShareActivity.java
+++ app/src/main/java/com/asus/instagramclone/ShareActivity.java
@@ -5,12 +5,21 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
 
+import com.asus.instagramclone.databinding.ActivityMainBinding;
+import com.asus.instagramclone.databinding.ActivityShareBinding;
+
 public class ShareActivity extends AppCompatActivity {
 
+    private ActivityShareBinding binding;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_share);
+        binding = ActivityShareBinding.inflate(getLayoutInflater());
+        View view = binding.getRoot();
+        setContentView(view);
+
+
+
     }
 
 
