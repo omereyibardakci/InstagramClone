589bac10c7a1848220dff0052cde43f688bd909a
Omer Eyibardakci <omereyibardakci@gmail.com>
2023-07-28 14:20:32 +0300

    Permissions completed

--- app/src/main/java/com/asus/instagramclone/ShareActivity.java
+++ app/src/main/java/com/asus/instagramclone/ShareActivity.java
@@ -1,16 +1,36 @@
 package com.asus.instagramclone;
 
+import androidx.activity.result.ActivityResult;
+import androidx.activity.result.ActivityResultCallback;
+import androidx.activity.result.ActivityResultLauncher;
+import androidx.activity.result.contract.ActivityResultContracts;
 import androidx.appcompat.app.AppCompatActivity;
+import androidx.core.app.ActivityCompat;
+import androidx.core.content.ContextCompat;
 
+import android.Manifest;
+import android.app.Activity;
+import android.content.Intent;
+import android.content.pm.PackageManager;
+import android.graphics.Bitmap;
+import android.net.Uri;
+import android.os.Build;
 import android.os.Bundle;
+import android.provider.MediaStore;
 import android.view.View;
+import android.widget.Toast;
 
 import com.asus.instagramclone.databinding.ActivityMainBinding;
 import com.asus.instagramclone.databinding.ActivityShareBinding;
+import com.google.android.material.snackbar.Snackbar;
 
 public class ShareActivity extends AppCompatActivity {
 
     private ActivityShareBinding binding;
+    ActivityResultLauncher<Intent> activityResultLauncher;
+    ActivityResultLauncher<String> permissionLauncher;
+    Uri imageData;
+
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -18,12 +38,94 @@ public class ShareActivity extends AppCompatActivity {
         View view = binding.getRoot();
         setContentView(view);
 
+        registerLauncher();
+
 
 
     }
 
 
     public void selectImage(View view){
+
+        if(Build.VERSION.SDK_INT>=Build.VERSION_CODES.TIRAMISU){
+            if (ContextCompat.checkSelfPermission(ShareActivity.this, Manifest.permission.READ_MEDIA_IMAGES) != PackageManager.PERMISSION_GRANTED){
+                if(ActivityCompat.shouldShowRequestPermissionRationale(ShareActivity.this,Manifest.permission.READ_MEDIA_IMAGES)){
+                    Snackbar.make(view,"Permisson needed for go to gallary.",Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            //request permission
+                            permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES);
+
+                        }
+                    }).show();
+                }else {
+                    // reguest permission
+                    permissionLauncher.launch(Manifest.permission.READ_MEDIA_IMAGES);
+                }
+            }else {
+                // go to gallary
+                Intent intentToGallary = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                activityResultLauncher.launch(intentToGallary);
+            }
+
+
+        }else {
+            if (ContextCompat.checkSelfPermission(ShareActivity.this, Manifest.permission.READ_EXTERNAL_STORAGE) != PackageManager.PERMISSION_GRANTED){
+                if(ActivityCompat.shouldShowRequestPermissionRationale(ShareActivity.this,Manifest.permission.READ_EXTERNAL_STORAGE)){
+                    Snackbar.make(view,"Permisson needed for go to gallary.",Snackbar.LENGTH_INDEFINITE).setAction("Give permission", new View.OnClickListener() {
+                        @Override
+                        public void onClick(View v) {
+                            //request permission
+                            permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+
+                        }
+                    }).show();
+                }else {
+                    // reguest permission
+                    permissionLauncher.launch(Manifest.permission.READ_EXTERNAL_STORAGE);
+                }
+            }else {
+                // go to gallary
+                Intent intentToGallary = new Intent(Intent.ACTION_PICK, MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                activityResultLauncher.launch(intentToGallary);
+            }
+
+
+        }
+
+    }
+
+
+    public void registerLauncher(){
+
+        activityResultLauncher = registerForActivityResult(new ActivityResultContracts.StartActivityForResult(), new ActivityResultCallback<ActivityResult>() {
+            @Override
+            public void onActivityResult(ActivityResult result) {
+                if (result.getResultCode() == Activity.RESULT_OK){
+                    Intent intentFromResult = result.getData();
+                    if (intentFromResult != null){
+                        imageData = intentFromResult.getData();
+                        binding.imageView.setImageURI(imageData);
+                    }
+                }
+            }
+        });
+
+
+        permissionLauncher = registerForActivityResult(new ActivityResultContracts.RequestPermission(), new ActivityResultCallback<Boolean>() {
+            @Override
+            public void onActivityResult(Boolean result) {
+                   if(result){
+                       // permission granted
+                       Intent intentToGallary = new Intent(Intent.ACTION_PICK,MediaStore.Images.Media.EXTERNAL_CONTENT_URI);
+                       activityResultLauncher.launch(intentToGallary);
+                   }else {
+                       // permission denied
+                       Toast.makeText(ShareActivity.this,"Permission needed for go to gallary.",Toast.LENGTH_LONG).show();
+                }
+            }
+        });
+
 
 
     }
